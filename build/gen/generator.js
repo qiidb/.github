ec39efe3c1133a5e2b7f74c6966fcf8d58ab945e
Ourai Lin <ourairyu@gmail.com>
2023-04-13 14:26:00 +0800

    fix(zh): meta fields for entries

--- build/gen/generator.js
+++ build/gen/generator.js
@@ -1,4 +1,5 @@
 const { safeDump } = require('js-yaml');
+const { pick } = require('@ntks/toolbox');
 
 const { ensureDirExists, getConfig, resolvePathFromRootRelative, resolvePathFromParams, saveData } = require('../helper');
 const { getSiteRoot, getLocalDataRoot, getLocalImageRoot, createGenerator } = require('./helper');
@@ -26,30 +27,37 @@ function resolveImagePath(srcPath, params, collectionName) {
 
   const options = {
     paramPath: paramPathMap[collectionName],
-    transformItem: (baseName, { id, slug, content = '', ...others }, params, cache) => {
-      cache.content = content.replace(/src=\"([^\"]+)\"/g, (match, srcPath) => match.replace(srcPath, resolveImagePath(srcPath, params, collectionName)))
+    transformItem: (baseName, { id, slug, content = '', ...others }, params) => {
+      const resolved = {
+        id: id || slug || baseName,
+        ...others,
+        content: content.replace(/src=\"([^\"]+)\"/g, (match, srcPath) => match.replace(srcPath, resolveImagePath(srcPath, params, collectionName)))
         .replace(/!\[([^\[\]]+)?\]\(([^\(\)]+)\)/g, (match, _, srcPath) => match.replace(srcPath, resolveImagePath(srcPath, params, collectionName)))
         .replace(/\n\`{3}([^\n]+)/g, (_, lang) => `\n{% highlight ${lang} %}`)
-        .replace(/\`{3}/g, '{% endhighlight %}');
+        .replace(/\`{3}/g, '{% endhighlight %}'),
+      };
 
-      return { ...others, id: id || slug || baseName };
+      if (!resolved.content) {
+        delete resolved.content;
+      }
+
+      return resolved;
     },
     transformData: items => ({ items: Object.entries(items).reduce((acc, [_, item]) => ({ ...acc, [item.id]: item }), {}) }),
-    readEach: (_, item, __, cache) => {
+    readEach: (_, item) => {
       if (!item) {
         return;
       }
 
-      const { id, content, ...data } = item;
+      const { id, ...data } = pick(item, ['id', 'title', 'description', 'date']);
 
       data.permalink = `/${collectionName}/${id}/`;
 
-      if (data.date) {
-        data.public_date = data.date;
+      if (collectionName !== 'routes') {
         delete data.date;
       }
 
-      saveData(`${localFileCollectionDir}/${id}.md`, `---\n${safeDump(data)}---\n\n${cache.content || content || ''}\n`);
+      saveData(`${localFileCollectionDir}/${id}.md`, `---\n${safeDump(data)}---\n`);
     },
   };
 
